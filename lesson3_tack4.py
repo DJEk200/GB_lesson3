34b09c7b3b0144684f8146e3d809bc346b8dff3f
DJEk200 <66725181+DJEk200@users.noreply.github.com>
2020-09-15 09:14:24 +0300

    Add files via upload

---
+++ lesson3_tack4.py
@@ -0,0 +1,14 @@
+""" Задача № 4
+Определить, какое число в массиве встречается чаще всего."""
+
+import random
+
+r = [random.randint(0, 10) for _ in range(50)]
+print(f'Массив: {r}')
+
+max_index = 0
+for i in r:
+    if r.count(max_index) < r.count(i):
+        max_index = r.index(i)
+
+print(f'Число {r[max_index]}, встречается {r.count(max_index)} раза')
